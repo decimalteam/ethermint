5ca1f43be86f7ec41d827da48b3c72341a6d6798
hakerkiler <vinatorul2@gmail.com>
2024-03-28 18:35:54 +0300

    add function to keeper for call contract

--- x/evm/keeper/utils.go
+++ x/evm/keeper/utils.go
@@ -16,6 +16,10 @@
 package keeper
 
 import (
+	"encoding/json"
+	"github.com/decimalteam/ethermint/server/config"
+	"github.com/ethereum/go-ethereum/accounts/abi"
+	"github.com/ethereum/go-ethereum/common/hexutil"
 	"math/big"
 
 	"github.com/ethereum/go-ethereum/common"
@@ -149,4 +153,86 @@ func CheckSenderBalance(
 		)
 	}
 	return nil
+}
+
+// CallEVM performs a smart contract method call using given args
+func (k *Keeper) CallEVM(
+	ctx sdk.Context,
+	abi abi.ABI,
+	from, contract common.Address,
+	commit bool,
+	method string,
+	args ...interface{},
+) (*types.MsgEthereumTxResponse, error) {
+	data, err := abi.Pack(method, args...)
+	if err != nil {
+		return nil, errorsmod.Wrap(
+			err,
+			errorsmod.Wrap(err, "failed to create transaction data").Error(),
+		)
+	}
+
+	resp, err := k.CallEVMWithData(ctx, from, &contract, data, commit)
+	if err != nil {
+		return nil, errorsmod.Wrapf(err, "contract call failed: method '%s', contract '%s'", method, contract)
+	}
+	return resp, nil
+}
+
+// CallEVMWithData performs a smart contract method call using contract data
+func (k *Keeper) CallEVMWithData(
+	ctx sdk.Context,
+	from common.Address,
+	contract *common.Address,
+	data []byte,
+	commit bool,
+) (*types.MsgEthereumTxResponse, error) {
+	nonce, err := k.accountKeeper.GetSequence(ctx, from.Bytes())
+	if err != nil {
+		return nil, err
+	}
+
+	gasCap := config.DefaultGasCap
+	if commit {
+		args, err := json.Marshal(types.TransactionArgs{
+			From: &from,
+			To:   contract,
+			Data: (*hexutil.Bytes)(&data),
+		})
+		if err != nil {
+			return nil, errorsmod.Wrapf(errortypes.ErrJSONMarshal, "failed to marshal tx args: %s", err.Error())
+		}
+
+		gasRes, err := k.EstimateGas(sdk.WrapSDKContext(ctx), &types.EthCallRequest{
+			Args:   args,
+			GasCap: config.DefaultGasCap,
+		})
+		if err != nil {
+			return nil, err
+		}
+		gasCap = gasRes.Gas
+	}
+
+	msg := core.Message{
+		From:              from,
+		Nonce:             nonce,
+		GasLimit:          gasCap,
+		GasPrice:          big.NewInt(0),
+		GasFeeCap:         big.NewInt(0),
+		GasTipCap:         big.NewInt(0),
+		To:                contract,
+		Value:             big.NewInt(0),
+		Data:              data,
+		AccessList:        ethtypes.AccessList{},
+		SkipAccountChecks: true,
+	}
+
+	res, err := k.ApplyMessage(ctx, msg, types.NewNoOpTracer(), commit)
+	if err != nil {
+		return nil, err
+	}
+	if res.Failed() {
+		return nil, errorsmod.Wrap(types.ErrVMExecution, res.VmError)
+	}
+	return res, nil
 }
